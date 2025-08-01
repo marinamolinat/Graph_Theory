97df6bf1ca4cc1144bda0b0f077da081fb903410
Marina M <marinamolina7877@gmail.com>
2025-08-01 11:39:02 -0500

    delete files, and started stylig the site

---
+++ planning.md
@@ -0,0 +1,32 @@
+
+
+oajygduaegjdged\
+
+1. use recurssion to solve this issue. Im honestly way to fucking lazy to google an optimal olgorithm
+
+
+function
+{
+    let Min-num = 0
+    let current num = 0
+    lsit of every node
+    for every node {
+        remove a node
+
+    check if it the set is domination --->
+        yes: 
+         dominationNum
+           if Min-num < dominationNum:
+                biggestNum = dominationNum;
+                print(biggestNum)
+
+        
+            function()
+
+        no: return false 
+
+    }
+    
+
+}
+
--- sw.js
+++ sw.js
@@ -1,49 +1,5 @@
 
-console.log("Hello, world!");
- const graph = new graphology.Graph({type: 'directed'});
-
-
-        // make the size of a board
-        let n = 3;
-        for (let i = 0; i < n; i++) {
-            for (let j = 0; j < n; j++) {
-                 graph.addNode(`(${j}, ${i})`, { x: j, y: i, size: 10, color: "red", label: `(${j}, ${i})` });
-
-            }
-          
-        }
-
-        const movement = [
-            [-1, 1], //diagonally  left
-            [1, 1], // diagonally right
-            
-        ];
-
-        graph.forEachNode((node, attributes) => {
-            
-
-            // Example: use attributes.x and attributes.y for movement
-            for (let [dx, dy] of movement) {
-                let newX = attributes.x + dx;
-                let newY = attributes.y + dy;
-
-                // Build ID of potential destination node
-                let toId = `(${newX}, ${newY})`;
-
-                if (graph.hasNode(toId)) {
-                graph.addDirectedEdge(node, toId, {type: 'arrow', size: 4});
-               
-                } 
-            }
-
-        });
-
-        //Superbad, brute force algorith to find domination number, please help, this will probably be shit
-        
-        //rn it just displays the size of A dominating set, not the domination number
-
-        
-        function isSetDominating(graph, nodes) {
+function isSetDominating(graph, nodes) {
             const visited = new Set();
             for (const node of nodes) {
                 //mark node as visited, if it hasnt been already marked
@@ -135,20 +91,3 @@ console.log("Hello, world!");
             }
 
         }
-
-
-       console.log("checking some stuff", noName(graph, graph.nodes()));
-       
-       
-
-
-    
-
-
-      // Instantiate sigma.js and render the graph
-      const sigmaInstance = new Sigma(graph, document.getElementById("container"));
-      
-     
-
-
- 
