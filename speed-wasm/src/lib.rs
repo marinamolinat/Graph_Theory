be6ad01946609b5fab87bfd32c104320d3e13282
Marina M <marinamolina7877@gmail.com>
2025-08-06 12:14:18 -0500

    I think i have the issetdomiantion function working in rust

--- speed-wasm/src/lib.rs
+++ speed-wasm/src/lib.rs
@@ -1,13 +1,38 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+pub fn isSetDominating(bitNodes: u64, order: u32, neighborhood: &[u64])-> bool {
+    let mut visited: u64 = 0;
+
+    
+
+    for  i in 0..order {
+    
+            
+        //Check if the i-th node is in the set, if it is, mark it as and its neighbours as visited
+        let check = (1u64) << (i as u64);
+        if (bitNodes & check != 0) {
+            visited |= check;
+            visited |= neighborhood[i as usize];
+
+
+
+        }
+
+    } 
+    if (visited == (1u64 << (order as u64)) - 1u64) {
+        return true;
+    }
+   false
+ 
+    
 }
 
 
 
 #[wasm_bindgen]
-pub fn findDominationNumber(bitNodes: u64, order: u32, neighborhood: &[u64]) -> u32{
-    order
+pub fn findDominationNumber(bitNodes: u64, order: u32, neighborhood: &[u64]) -> bool{
+    return isSetDominating(bitNodes, order, neighborhood);
+    
 }
+
+
